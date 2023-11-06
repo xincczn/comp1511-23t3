06880a7ce950e88a31db01ee6c86a79920bbb4a1
Xing He <z5413977@ad.unsw.edu.au>
2023-11-06 15:41:49 +1100

    feat: impl tut 8

--- tut_08/linked_list.c
+++ tut_08/linked_list.c
@@ -1,71 +1,101 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+// Linked List Node
 struct node {
-    int data;
+    // Node Values
+    int value;
+
+    // Next Pointer
     struct node *next;
 };
 
+// Linked List functions
+struct node *create_node(int value);
+struct node *append_to_head(struct node *new_node, struct node *head);
+struct node *append_to_tail(struct node *new_node, struct node *head);
+void print_list(struct node *head);
+
+// Main
+int main(void) {
+    // New empty list
+    struct node *head = NULL;
+
+    // Created a bunch of nodes
+    struct node *node1 = create_node(1);
+    struct node *node2 = create_node(2);
+    struct node *node3 = create_node(3);
+
+    // Insert to head
+    head = append_to_head(node1, head);
+    head = append_to_head(node2, head);
+    head = append_to_head(node3, head);
+
+    // Create more nodes
+    struct node *node4 = create_node(4);
+    struct node *node5 = create_node(5);
+    struct node *node6 = create_node(6);
+
+    // Insert to tail
+    head = append_to_tail(node4, head);
+    head = append_to_tail(node5, head);
+    head = append_to_tail(node6, head);
+
+    print_list(head);
+
+    return 0;
+}
+
+// Creates a new node
 struct node *create_node(int value) {
     struct node *new_node = malloc(sizeof(struct node));
-    new_node->data = value;
-    new_node->next = NULL;
+    new_node->value = value;
+    // Remember to always set new node's next to null initially
+    // Eliminates errors where next is uninitialised
+    new_node->next = NULL;  
 
     return new_node;
 }
 
-void print_list(struct node *head) {
-
-    struct node *current = head;
-    while (current != NULL) {
-        printf("%d -> ", current->data);
-        current = current->next;        
+// Inserts node as new head of list
+struct node *append_to_head(struct node *new_node, struct node *head) {
+    // Empty List Case
+    if (head == NULL) {
+        // Remember that single nodes are just linked list with 1 element
+        return new_node;
     }
-    printf("X\n");
 
+    new_node->next = head;
+    return new_node;
 }
 
-struct node *insert_head(struct node *head, struct node *new) {
+// Inserts node as new tail of list
+struct node *append_to_tail(struct node *new_node, struct node *head) {
+    // Empty List Case
     if (head == NULL) {
-        return new;
+        return new_node;
     }
 
-    new->next = head;
-    return new;
-}
-
-void insert_tail(struct node *head, struct node *new) {
-    if (head == NULL) {
-        return new;
-    }
-    
+    // Loop that positions `current` to the tail of the list
     struct node *current = head;
+    // Note that this loop while fail if the base case isn't handled
     while (current->next != NULL) {
         current = current->next;
     }
 
-    current->next = new;
+    current->next = new_node;
+    return head;
 }
 
-int main() {
-
-    struct node *node_1 = create_node(10);
-    struct node *node_2 = create_node(20);
-    struct node *node_3 = create_node(30);
-
-    node_1->next = node_2;
-    node_2->next = node_3;
-
-    print_list(node_1);
-
-    struct node *new = create_node(0);
-    struct node *list = node_1;
-
-    list = insert_head(list, new);
-
-    struct node *new_2 = create_node(40);
-    insert_tail(list, new_2);
-
-
+// Prints list out
+void print_list(struct node *head) {
+    // Standard iterating structure for linked lists
+    struct node *current = head;
+    while (current != NULL) {
+        // Prints individual node's value, formatted by an arrow 
+        printf("%d -> ", current->value);
+        current = current->next;
+    }
+    // We can't physically print NULL's value, so we print a X
+    printf("X\n");  
 }
-
--- tut_08/malloc.c
+++ tut_08/malloc.c
@@ -1,34 +1,75 @@
 #include <stdio.h>
-#include <stdlib.h>
+#include <stdlib.h>  // malloc is defined in stdlib
 
-int array[10]
+#define ARRAY_SIZE 3
+
+struct box {
+    int item_i;
+    char item_c;
+    double item_d;
+};
 
 
-int *create_array() {
-    // size of array * size of the type
-    int *array = malloc(5 * sizeof(int));
-    array = realloc(array, 20 * sizeof(int))
-
-    for (int i = 0; i < 5; i++) {
-        array[i] = i + 1;
-    }
-
-    return array;
-}
-
+// Main
 int main() {
+    //========== What is malloc? ==========
+    /*
+        <return_type> <function_name>(<parameters>);
 
-    int *array = create_array();
-    for (int i = 0; i < 5; i++) {
-        printf("%d ", array[i]);
+        void *malloc(size_t size);
+        
+        void * -> Returns a pointer to the allocated memory
+        size_t -> type which is a funky alias for int
+        size   -> A number for amount of memory to allocate
+    */
+
+    // We can use sizeof(data_type) to determine the size of a type
+    // Don't worry about %lu, its just to print funky size_t types
+    printf("Size of Int: %lu\n", sizeof(int));
+    printf("Size of Char: %lu\n", sizeof(char));
+    printf("Size of Double: %lu\n", sizeof(double));
+
+    //========== Allocate memory for primitive data types ==========
+    double num = 42.42;
+
+    // Use malloc to allocate enough memory for an integer
+    double *num_d = malloc(sizeof(double));
+    *num_d = 18.18;
+
+    printf("Static memory: %.2lf\n", num);
+    printf("Dynamic memory: %.2lf\n", *num_d);
+
+    //========== Allocate memory for an array ==========
+
+    // Size of array -> amount of elements * size of an element
+    int *array_d = malloc(ARRAY_SIZE * sizeof(int));
+
+    array_d[0] = 1;
+    array_d[1] = 2;
+    array_d[2] = 3;
+
+    printf("Dynamic Array: [ ");
+    for (int i = 0; i < ARRAY_SIZE; i++) {
+        printf("%d ", array_d[i]);
     }
-    printf("\n");
-    free(array);
+    printf("]\n");
 
+    // Prove we allocated the right amount of memory
+    // printf("Shouldn't compile: %d\n", array_d[3]);
 
-    // printf("Size of Char: %lu\n", sizeof(char));
-    // printf("Size of Int: %lu\n", sizeof(int));
-    // printf("Size of Double: %lu\n", sizeof(double));
-    // printf("Size of Pointers: %lu %lu %lu\n", sizeof(char *), sizeof(int *), sizeof(double *));
+    //========== Allocate memory for a struct ==========
+    // Size of struct box
+    printf("Size of Struct Box: %lu\n", sizeof(struct box));
 
+    // Allocate like before, use -> instead of . since struct pointer
+    struct box *new_box = malloc(sizeof(struct box));
+    new_box->item_i = 1;
+    new_box->item_c = '2';
+    new_box->item_d = 3.0;
+
+    printf("Dynamic Struct Box: {\n");
+    printf("\titem_i: %d\n", new_box->item_i);
+    printf("\titem_c: %c\n", new_box->item_c);
+    printf("\titem_d: %.1lf\n", new_box->item_d);
+    printf("}\n");
 }
